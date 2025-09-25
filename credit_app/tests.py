36247137fc160778793751161112a55fdf23e15b
CoderNikkcoder <159905764+CoderNikkcoder@users.noreply.github.com>
2025-09-25 15:07:36 +0530

    tests.py

--- credit_app/tests.py
+++ credit_app/tests.py
@@ -9,7 +9,7 @@ class RegistrationAPITest(APITestCase):
         """
         Ensure we can create a new customer object successfully.
         """
-        # 'register' naam humne credit_app/urls.py me diya tha
+  
         url = reverse('register') 
         
         data = {
@@ -20,21 +20,20 @@ class RegistrationAPITest(APITestCase):
             "phone_number": 9876543210
         }
         
-        # API ko call karo
+   
         response = self.client.post(url, data, format='json')
         
-        # Check karo ki response sahi hai ya nahi
-        # 1. Status code 201 Created hona chahiye
+       
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         
-        # 2. Database me ek customer ban jaana chahiye
+       
         self.assertEqual(Customer.objects.count(), 1)
         
-        # 3. Jo customer bana hai, uska data sahi hona chahiye
+        
         customer = Customer.objects.get()
         self.assertEqual(customer.first_name, "Test")
         
-        # 4. Check karo ki approved_limit sahi calculate hui hai (36 * 50000 = 18,00,000)
+    
         self.assertEqual(response.data['approved_limit'], 1800000)
 
     def test_register_customer_failure_missing_field(self):
@@ -50,5 +49,5 @@ class RegistrationAPITest(APITestCase):
         }
         response = self.client.post(url, data, format='json')
         
-        # Check for a 400 Bad Request status code
+       
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
